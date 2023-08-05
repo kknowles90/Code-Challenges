da4aa264d4508913ced4b6329758661d3a9fa05e
kknowles90 <85813787+kknowles90@users.noreply.github.com>
2023-08-04 21:49:18 -0400

    Add files via upload

---
+++ codingGame/codingGame/closestTempToZero.cpp
@@ -0,0 +1,56 @@
+﻿/*
+Write a program that prints the temperature closest to 0 among input data. If two numbers are equally close to zero, positive integer has to be considered closest to zero (for instance, if the temperatures are -5 and 5, then display 5).
+    Game Input
+Your program must read the data from the standard input and write the result on the standard output.
+Input
+Line 1: N, the number of temperatures to analyze
+
+Line 2: A string with the N temperatures expressed as integers ranging from -273 to 5526
+
+Output
+Display 0 (zero) if no temperatures are provided. Otherwise, display the temperature closest to 0.
+Constraints
+0 ≤ N < 10000
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+int main()
+{
+    int n; // the number of temperatures to analyse
+    cin >> n;
+    cin.ignore();
+
+    if (n == 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    int closestTemp = 5526; // Initialize with a large positive number
+    for (int i = 0; i < n; i++) {
+        int t; // a temperature expressed as an integer ranging from -273 to 5526
+        cin >> t;
+        cin.ignore();
+
+        if (abs(t) < abs(closestTemp) || (abs(t) == abs(closestTemp) && t > closestTemp))
+        {
+            closestTemp = t;
+        }
+    }
+
+    // Write an answer using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << closestTemp << endl;
+}
